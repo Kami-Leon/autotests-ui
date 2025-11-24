acfa353a91939fdf1c69d5ad6bc09498fb25289e
Your Name <you@example.com>
2025-11-24 20:32:56 +0300

    6.5 Фикстуры pytest. Практика работы с pytest фикстурами

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,7 +32,7 @@ def initialize_browser_state(playwright: Playwright):
     context.storage_state(path='browser-state.json')
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state='browser-state.json')
