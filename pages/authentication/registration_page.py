fabe89745a66aa195a6b629ccc1dab5d6bf28a0b
Your Name <you@example.com>
2025-12-03 19:26:23 +0300

    Наращивание тестовой базы и рефакторинг

--- pages/authentication/registration_page.py
+++ pages/authentication/registration_page.py
@@ -1,4 +1,6 @@
-from playwright.sync_api import Page, expect
+import re
+
+from playwright.sync_api import Page
 
 from components.authentication.registration_form_component import RegistrationFormComponent
 from elements.button import Button
@@ -22,3 +24,4 @@ class RegistrationPage(BasePage):
 
     def click_login_link(self):
         self.login_link.click()
+        self.check_current_url(re.compile('.*/#/auth/login'))
